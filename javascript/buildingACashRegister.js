b81093c649654cfc2080a9f90adb55a4e6cbd1f8
Michael Brett <mdouglasbrett@icloud.com>
2015-11-30 18:55:35 +0000

    Replaced StaffMember() with newEmployee()

--- javascript/buildingACashRegister.js
+++ javascript/buildingACashRegister.js
@@ -1,22 +1,27 @@
-function StaffMember(name,discountPercent){
-    this.name = name;
-    this.discountPercent = discountPercent;
+'use strict';
+
+function employee(name, discount) {
+	var employeeObj = {
+		'name': name,
+		'discount': discount
+	}
+	return employeeObj;
 }
 
-var sally = new StaffMember("Sally",5);
-var bob = new StaffMember("Bob",10);
-
-// Create yourself again as 'me' with a staff discount of 20%
-var me = new StaffMember("Mike", 20);
+var sally = employee('Sally', 5);
+var bob = employee('Bob', 10);
+var mike = employee('Mike', 20);
 
 var cashRegister = {
-    total:0,
-    lastTransactionAmount: 0,
+    'total': 0,
+    'lastTransactionAmount': 0,
+    
     add: function(itemCost){
         this.total += (itemCost || 0);
         this.lastTransactionAmount = itemCost;
     },
-    scan: function(item,quantity){
+    
+    scan: function(item, quantity){
         switch (item){
         case "eggs": this.add(0.98 * quantity); break;
         case "milk": this.add(1.23 * quantity); break;
@@ -25,13 +30,14 @@ var cashRegister = {
         }
         return true;
     },
+    
     voidLastTransaction : function(){
         this.total -= this.lastTransactionAmount;
         this.lastTransactionAmount = 0;
     },
-    // Create a new method applyStaffDiscount here
+    
     applyStaffDiscount : function(employee){
-   	this.total -= (this.total*(employee.discountPercent/100));
+   	this.total -= (this.total*(employee.discount / 100));
     }
     
 };
@@ -39,9 +45,31 @@ var cashRegister = {
 cashRegister.scan('eggs',1);
 cashRegister.scan('milk',1);
 cashRegister.scan('magazine',3);
+cashRegister.applyStaffDiscount(sally);
+
+// Show the total bill
+console.log('Your bill is '+cashRegister.total.toFixed(2));
+
+cashRegister.voidLastTransaction();
+
+cashRegister.scan('eggs',1);
+cashRegister.scan('milk',1);
+cashRegister.scan('magazine',3);
 // Apply your staff discount by passing the 'me' object 
 // to applyStaffDiscount
-cashRegister.applyStaffDiscount(me);
+cashRegister.applyStaffDiscount(bob);
+
+// Show the total bill
+console.log('Your bill is '+cashRegister.total.toFixed(2));
+
+cashRegister.voidLastTransaction();
+
+cashRegister.scan('eggs',1);
+cashRegister.scan('milk',1);
+cashRegister.scan('magazine',3);
+// Apply your staff discount by passing the 'me' object 
+// to applyStaffDiscount
+cashRegister.applyStaffDiscount(mike);
 
 // Show the total bill
 console.log('Your bill is '+cashRegister.total.toFixed(2));
