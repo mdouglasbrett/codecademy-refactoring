92a5eac62df18a57dac4b6058ee77f9b9e8f1ed9
Michael Brett <mdouglasbrett@icloud.com>
2015-12-01 13:42:36 +0000

    Cleaned up if/else, removed superfluous == 1

--- javascript/dragonSlayer.js
+++ javascript/dragonSlayer.js
@@ -1,19 +1,21 @@
-laying = true;
+'use strict';
+
+var slaying = true;
 var youHit = Math.floor(Math.random() * 2);
-var damageThisRound = Math.floor(Math.random() *5+1);
+var damageThisRound = Math.floor((Math.random() * 5)+1);
 var totalDamage = 0; 
 
 while(slaying) {
-	if (youHit == 1) {
-		console.log("Congratulations, you killed the dragon!");
+	if (youHit) {
+		console.log("A touch! You smote the dragon!");
 		totalDamage += damageThisRound;
 		if (totalDamage >= 4) {
-			console.log("Congratulations, you killed the dragon!");
-		} else {
-			youHit = Math.floor(Math.random() * 2);
-		}
-	} else {
-		console.log ("Sorry pal, you're toast!");
+			console.log("The dragon is dead, huzzah!");
+			slaying = false;
 	}
-	slaying = false;
+	}
+	else {
+		console.log ("Sorry pal, you're toast!");
+        	slaying = false;
+	}
 }
