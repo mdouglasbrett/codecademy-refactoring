377e38dc623436c068657a716f864a44afd8cd15
Michael Brett <mdouglasbrett@icloud.com>
2015-12-08 19:08:57 +0000

    Re-did search(), so that it (sort-of) works. This is rough

--- javascript/contactList.js
+++ javascript/contactList.js
@@ -1,29 +1,41 @@
+'use strict';
+
 var friends = {
-bill: {
-firstName: "Bill",
-lastName: "Gates",
-number: "0207 352 6040",
-address: ['One Microsoft Way', 'Redmond', '98052']
-},
-steve: {
+	bill: {
+	firstName: "Bill",
+	lastName: "Gates",
+	number: "0207 352 6040",
+	address: ['One Microsoft Way', 'Redmond', '98052']
+	},
+	steve: {
 	firstName: "Steve",
 	lastName: "Jobs",
 	number: "0207 590 9040",
 	address: ['300 Kings Road', 'Chelsea', 'London', 'SW3 5UH']
-}
+	}
 };
-var list = function() {
+
+function list() {
+
 	for (var firstName in friends) {
 		console.log(firstName);
 	}
-};
-var search = function(name) {
-	for (var search in friends) {
 
-		console.log(friends);
-		return friends;
+}
+
+function search(name) {
+	
+	for (var key in friends) {
+		if (friends[key].firstName == name) {
+
+		console.log(friends[key]);
+		return friends[key];
+	} else {
+		console.log('Not found');
 	}
 	
-};
+}
+}
 
-
+list();
+search('Bill');
